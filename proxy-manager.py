fea1b0b097b556503a5f6b66f32352d55349d7e4
Alin Banuta <alin.alex.b@gmail.com>
2017-07-19 15:07:59 +0300

    Abstraction on requests module
    added getattr, added all functions to a class, added maxrotations

--- proxy-manager.py
+++ proxy-manager.py
@@ -1,46 +1,103 @@
 import requests
 from queue import Queue
 
-proxies = ["61.183.8.51:3128",
-			"175.155.24.19:808"]
 
-invalidStringList = ["Inserisci i caratteri visualizzati nello spazio sottostante","To discuss automated access to Amazon data please contact","For automated access to price change or offer listing change events","Our systems have detected unusual traffic from your computer network","please type the characters below","Permission denied","Geben Sie die angezeigten",'hogy szokatlanul nagy a forgalom az oldalon','Validare acces pagina','Te rugam sa verifici pentru a continua','500 Service Unavailable Error','banned your IP', 'DDoS protection by CloudFlare', 'To continue, please type the characters below', 'your computer or network may be sending automated queries','Enter the characters you see below']
+class proxyManager:
+	def __init__(self):
+		self.proxies = ["121.244.91.62:3128",
+			            "104.154.205.214:1080",
+			            "200.108.35.60:8087",
+			            "180.250.159.67:80",
+			            "67.205.179.195:3128",
+			            "180.250.159.69:80",
+			            "137.59.44.47:8080",
+			            "199.193.188.94:80",
+			            "195.190.124.202:8080",
+			            "150.187.5.100:8080",
+			            "96.9.250.117:8888",
+			            "109.75.254.139:8080",
+			            "176.62.77.212:8080",
+			            "95.143.192.200:80",
+			            "46.163.119.138:3128",
+			            "185.145.129.106:8080",
+			            "144.217.248.180:80",
+			            "47.52.24.117:80",
+			            "189.40.191.95:8080",
+			            "199.193.188.84:80"]
+			            # https://free-proxy-list.net/
+		self.invalidStringList = ["500 Internal Server Error","You’re not barking up the wrong tree",
+									"not allowed to access this page","Our systems have detected unusual traffic from your computer network",
+									"please type the characters below","Permission denied",
+									"Geben Sie die angezeigten",'hogy szokatlanul nagy a forgalom az oldalon',
+									'Validare acces pagina',
+									'Te rugam sa verifici pentru a continua',
+									'500 Service Unavailable Error',
+									'banned your IP',
+									'DDoS protection by CloudFlare',
+									'To continue, please type the characters below',
+									'your computer or network may be sending automated queries','Enter the characters you see below']
+		self.proxiesQueue = Queue()
+		self.loadProxiesToQueue()
+		self.maxRotations = len(self.proxies)
+		self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
 
-def isBanned(response):
-	for keyword in invalidStringList:
-		if keyword in response.text:
-			print("Found banning phrase:{}".format(keyword))
-			return True
-	return False
 
-def rotate(proxiesQueue, currentProxy):
-	newProxy = proxiesQueue.get()
-	proxiesQueue.put(currentProxy)
-	proxy = proxy = { "http" : newProxy }
-	return proxiesQueue, proxy
-	
+	def isBanned(self,response):
+		for keyword in self.invalidStringList:
+			if keyword in response.text:
+				print("Found banning phrase:{}".format(keyword))
+				return True
+		return False
 
-def loadProxiesToQueue():
-	proxiesQueue = Queue()
-	for proxy in proxies:
-		proxiesQueue.put(proxy)
-	return proxiesQueue
+	def rotate(self, currentProxy):
+		newProxy = self.proxiesQueue.get()
+		self.proxiesQueue.put(currentProxy)
+		proxy  = { "http" : newProxy }
+		return proxy
+		
 
-def test(url):
-	proxiesQueue = loadProxiesToQueue()
-	newProxy = proxiesQueue.get()
-	proxy = { "http" : newProxy }
-	print(proxy)
-	response = requests.get(url, proxies = proxy)
-	while isBanned(response):
-		print("Making rotation")
-		proxiesQueue, proxy = rotate(proxiesQueue, newProxy)
+	def loadProxiesToQueue(self):
+		for proxy in self.proxies:
+			self.proxiesQueue.put(proxy)
+
+	def makeRequest(self,*args,**kwargs):
+		requestsMethod = kwargs.pop("requestsMethod")
+		newProxy = self.proxiesQueue.get()
+		proxy = { "http" : newProxy }
 		print(proxy)
-		response = requests.get(url, proxies = proxy)
-	print("Success")
+		kwargs["proxies"] = proxy
+		response = requestsMethod(*args, **kwargs)
+		i = 1
+		while self.isBanned(response) and i<= self.maxRotations:
+			print("Making rotation")
+			proxy = self.rotate(newProxy)
+			print(proxy)
+			kwargs["proxies"] = proxy
+			response = requestsMethod(*args, **kwargs)
+			i += 1
+		return response
+		# print(response.text)
 
+
+	def __getattr__(self,name):
+		def handlerFunction(*args,**kwargs):
+			if 'headers' not in kwargs:
+				kwargs['headers'] = self.headers
+			kwargs["requestsMethod"] = getattr(requests, name)
+			try:
+				response = self.makeRequest(*args, **kwargs)
+			except requests.exceptions.ProxyError as e:
+				print(e)
+				response = requests.Response()
+
+			return response
+		return handlerFunction
+
+#TODO: check for long response proxies or dead ones
 
 if __name__ == '__main__':
-	url = "https://www.amazon.de/HP-Officejet-Multifunktionsdrucker-kompatibel-4800x1200/dp/B00VRNL4TA/ref=cm_cr_arp_d_product_top?ie=UTF8"
+	url = "https://www.yelp.com/biz/changs-kitchen-san-francisco?osq=delivery"
+	manager = proxyManager()
+
 	for i in range(0,10):
-		test(url)
+		manager.get(url)
